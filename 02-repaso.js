f83509f6cebf27849208c61a05cc428b781f112d
Daniela-Pozo <garciapozoerikadaniela>
2024-08-22 18:00:05 -0600

    se agregaron nuevos ejemplos de iteraciones

--- 02-repaso.js
+++ 02-repaso.js
@@ -2,10 +2,62 @@
 
 array= [[3,4,5,6],[7,8,9]];
 
-const datosArreglo= ()=>{
+/*const datosArreglo= ()=>{
     array.map((datos)=>{
         datos.map((valores, i)=>{
             console.log(`${i}=${valores}`);
         })
     });
 }
+
+datosArreglo();*/
+
+/*for of
+const datosArreglo= () => {
+    for ( const datos of array){
+        for (const valores of datos){
+            console.log(`${datos.indexOf(valores)}=${valores}`)
+        }
+    }
+}  
+
+datosArreglo();*/
+
+  
+//for in
+/*const datosArreglo = () => {
+    for (const datosIndex in array){
+         for (const valoresIndex in array[datosIndex]) {
+             console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+         }
+    }
+}
+
+datosArreglo();*/
+
+
+//funcion flecha
+/*const datosArreglo = () => {
+    for (const datosIndex in array) {
+       for(const valoresIndex in array[datosIndex]) 
+          console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+    }
+}
+
+datosArreglo();*/
+
+
+//while
+const datosArreglo = () => {
+    let datosIndex = 0;
+    while (datosIndex < array.length) {
+        let valoresIndex = 0;
+        while (valoresIndex < array[datosIndex].length) {
+            console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+             valoresIndex++;
+        }
+        datosIndex++;
+    }
+
+}
+datosArreglo();
