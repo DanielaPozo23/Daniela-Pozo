7eca69c9de6031da9c11f4e5be6d97fa11c6cd33
Daniela-Pozo <garciapozoerikadaniela>
2024-10-21 19:20:54 -0600

    trabajos de React

--- proyecto/proyecto/src/App.js
+++ proyecto/proyecto/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Otro from "./Otro";
+import Formulariocss from './Formulariocss';
+import Formularioboot from './components/Formularioboot';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
+  
+      <Otro />
+      <Formulariocss/>
+      <div className='container-fluid p-3'></div>
+      <Formularioboot/>
     </div>
   );
 }
---
+++ proyecto/proyecto/src/Otro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Otro() {
+  return (
+    <div>Otro Componente</div>
+  )
+}
+
+export default Otro
